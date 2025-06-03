2e4a167955f40c8a0ad9a28332b6d63a4eb057f9
average-akil <ahsoykat22@gmail.com>
2025-06-03 06:49:13 +0600

    3rd commit

--- js/addDonation.js
+++ js/addDonation.js
@@ -1,5 +1,31 @@
 document.getElementById('btn-donation').addEventListener('click', function (event) {
+
     event.preventDefault();
+    //donation ja dibo input dilam
+
     const donationValue = getInputFieldValueById('input-donation');
-    console.log('got the value', donationValue)
+    //donation already ja dewa ase ta input nilam
+
+    const donationGiven = getInputTextValueById('1st-donation-amount');
+    //main balance ta update korar jnno nilam
+    const mainBlance = getInputTextValueById('main-balance')
+
+    if (typeof donationGiven == "number" && donationGiven > 0) {
+        if (donationValue <= mainBlance) {
+            // jototuk donation dicchi oita real time e update hocche
+            const newAmount = donationValue + donationGiven;
+            document.getElementById('1st-donation-amount').innerText = newAmount
+            // main balance theke donation kata niye kaaj
+            //main balance  e enough taka na hoile balance update korbe na
+
+            const newMainBlance = mainBlance - donationValue;
+            document.getElementById('main-balance').innerText = newMainBlance;
+        } else {
+            alert('eto teka nai')
+        }
+
+    } else {
+        alert('fatrami koriyen na')
+    }
+
 })
